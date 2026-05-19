7a823262dca7b676fc2cc207462112fb57600146
Mahamudur10 <mahamudur649@gmail.com>
2026-05-20 01:15:09 +0600

    Add POST /cars endpoint to insert car data into MongoDB collection

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const dns = require("node:dns");
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 const express = require('express')
 const dotenv = require('dotenv')
+const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require('mongodb');
 dotenv.config()
 const uri = process.env.MONGODB_URI;
 
 const app = express()
 const PORT = process.env.PORT;
+app.use(cors())
+app.use(express.json())
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -19,14 +22,27 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // Send a ping to confirm a successful connection
+
+        const db = client.db("carvio")
+        const carsCollection = db.collection("cars")
+
+        app.post('/cars', async (req, res) => {
+            const carData = req.body
+            console.log(carData)
+            const result = await carsCollection.insertOne(carData)
+
+            res.json({
+                success: true,
+                message: "Car added successfully",
+                carId: result.insertedId
+            })
+        })
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
+
     }
 }
 run().catch(console.dir);
